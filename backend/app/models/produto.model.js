5bfd4bed70494f06b58688eb3f48db5e19cb85fb
Amandha Kae <amandhakae@gmail.com>
2023-05-31 14:55:03 -0300

    Projeto Lanchonete

--- backend/app/models/produto.model.js
+++ backend/app/models/produto.model.js
@@ -1,17 +1,44 @@
 const sql = require("./db.js");
 //construtor
 const ProdutoModel = function(produto){
-    this.name = produto.nome;
+    this.nome = produto.nome;
     this.valor = produto.valor;
 }
 
 //cria novo pedido no banco
 ProdutoModel.create = (produto, result) => {
+
+    sql.query("INSERT into produtos set ?", produto, (err, res) => {
+        if (err) {
+            console.log("erro: ", err);
+            result(null, err);
+            return;
+        }
+        
+        console.log("produto criado: ",{idprodutos: res.insertId, ...produto});
+        result(null, {idprodutos: res.insertId, ...produto});
+    })
 };
 
 //seleciona produto por ID
 ProdutoModel.findById = (produtoId, result) => {
-};
+
+        sql.query("Select * from produtos where idprodutos ="+produtoId, (err,res) => {
+            if (err) {
+                console.log("erro: ", err);
+                result(null,err);
+                return;
+            }
+            if (res.length) {
+                console.log("Produto Encontrado", res[0]);
+                result(null,res[0]);
+            } else {
+                result({type: "not_found"}, null);
+                console.log("Produto não encontrado");
+            }
+        })
+    };
+
 
 //seleciona todos os produtos
 ProdutoModel.getAll = result =>{
@@ -29,7 +56,18 @@ ProdutoModel.getAll = result =>{
 //Atualizar produto por ID
 
 ProdutoModel.updateById = (produtoId, produto, result) => {
-
+    sql.query("UPDATE produtos SET nome = ?, valor = ? WHERE idprodutos = ? ",
+    [produto.nome, produto.valor, produtoId], (err, res) => {
+        if (err){
+            console.log("erro: ", err);
+            result(null, err);
+        } else if (res.affectedRows == 0){
+            result({type: "not_found"}, null);
+        } else {
+            console.log("Produto atualizado: ", {idprodutos: produtoId, ...produto});
+            result(null, {idprodutos: produtoId, ...produto});
+        }
+    });
 };
 
 //Remover produto por ID
